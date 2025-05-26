205cde10d4a006fd2b2971b3fd24a663cf79bb66
Vasyl <vasyl@example.com>
2025-05-27 01:19:08 +0300

    points

--- django_drf_backend/facilities/admin.py
+++ django_drf_backend/facilities/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from .models import FacilitesPoligons
+from .models import FacilitesPoligons, FacilitesPoints
 
 
 class FacilitiesAdmin(LeafletGeoAdmin):
     list_display = ("name", "description")
 
 
-
 admin.site.register(FacilitesPoligons, FacilitiesAdmin)
+admin.site.register(FacilitesPoints, FacilitiesAdmin)
--- django_drf_backend/facilities/serializers.py
+++ django_drf_backend/facilities/serializers.py
@@ -1,6 +1,6 @@
 # serializers.py
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from .models import FacilitesPoligons
+from .models import FacilitesPoligons, FacilitesPoints
 
 
 class FacilitesPoligonsSerializer(GeoFeatureModelSerializer):
@@ -8,3 +8,10 @@ class FacilitesPoligonsSerializer(GeoFeatureModelSerializer):
         model = FacilitesPoligons
         geo_field = "geom"  # Вказуємо поле геометрії
         fields = ("id", "name", "description", "geom")
+
+
+class FacilitesPointsSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = FacilitesPoints
+        geo_field = "geom_point"  # Вказуємо поле геометрії
+        fields = ("id", "name", "description", "geom_point")
